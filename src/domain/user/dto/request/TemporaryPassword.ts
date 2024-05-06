fcb217c5212dc050ddf1b02a37326ffb4ab64201
seungryeol <opop0421@naver.com>
2024-05-07 02:19:28 +0900

    [fix] 비번 생성 위치 변경

---
+++ src/domain/user/dto/request/TemporaryPassword.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+
+export class TemporaryPassword{
+
+    @IsNotEmpty()
+    @IsEmail()
+    private email: string;
+
+
+    @IsNotEmpty()
+    private identifier: string;
+
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public getIdentifier(): string {
+        return this.identifier;
+    }
+
+}
