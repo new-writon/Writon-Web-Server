b4efa8e8c03f590ac3f63d0226ce7b13b7a8a758
seungryeol <opop0421@naver.com>
2024-05-07 01:37:39 +0900

    [feat] 유효성 검사 에러처리

--- src/domain/user/exception/UserErrorCode.ts
+++ src/domain/user/exception/UserErrorCode.ts
@@ -5,7 +5,9 @@ export enum UserErrorCode{
     ERROR = 501,
     NO_VALUE=502,
     INVALIDATE_IDENTIFIER=450,
-    INVALIDATE_EMAIL=451
+    INVALIDATE_EMAIL=451,
+    NOT_VERIFY_CODE=452,
+    NOT_VERIFY_EMAIL=453
 
     
 }
@@ -15,7 +17,9 @@ const ErrorMessages: { [key: number]: string } = {
     501: "강제 에러 발생",
     502: "해당 값이 존재하지 않습니다.",
     450: "아이디가 중복됩니다.",
-    451: "이메일이 중복됩니다."
+    451: "이메일이 중복됩니다.",
+    452: "인증 코드가 옳바르지 않습니다.",
+    453: "이메일이 옳바르지 않습니다."
 
 };
 
