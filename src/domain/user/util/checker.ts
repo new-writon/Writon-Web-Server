99375ddcd6e8a84bcb486a4954b2fc8d978d869d
seungryeol <opop0421@naver.com>
2024-05-09 02:36:55 +0900

    [feat] 데이터 체커 추가

---
+++ src/domain/user/util/checker.ts
@@ -0,0 +1,18 @@
+
+     /**
+     * 
+     * @param data 
+     * @returns  데이터가 없을 경우 false 반환, 있을 경우 true 반환
+     */
+    const checkData = (data: any): boolean => {
+        let result = true
+        if (!data) {   // 데이터가 없을 경우
+            return result = false;
+        }
+        return result;
+    }
+
+
+    export {
+        checkData
+    }
