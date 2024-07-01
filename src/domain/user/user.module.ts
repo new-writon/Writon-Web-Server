8560e815b4f94519a139d46abd44a2401974f7bb
seungryeol <opop0421@naver.com>
2024-07-01 23:46:32 +0900

    [feat] 인프라 시도

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserController } from './presentation/User.Controller.js';
 import { User } from './domain/entity/User.js';
+
 import { UserService } from './service/User.Service.js';
 import { Affiliation } from './domain/entity/Affiliation.js';
 import { Organization } from './domain/entity/Organization.js';
