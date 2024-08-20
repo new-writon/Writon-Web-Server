072d96281aae6b98bb2aee21e053b66f6521ec31
seungryeol <opop0421@naver.com>
2024-08-20 11:50:53 +0900

    [fix] 메인 코드 수정

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import type { ConfigObject } from './global/config/configuration';
 import { HttpExceptionFilter } from './global/exception/HttpExceptionFilter';
-import { JwtService } from '@nestjs/jwt';
-import { TokenInterceptor } from './domain/auth/interceptors/Token.Interceptor';
+
 
 
 
