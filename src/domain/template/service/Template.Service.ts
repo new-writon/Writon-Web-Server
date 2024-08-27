bc04b5e1b12fe15dccde1378b56b553a9c853f0e
seungryeol <opop0421@naver.com>
2024-08-27 17:16:06 +0900

    [fix] 매핑 함수 변경

--- src/domain/template/service/Template.Service.ts
+++ src/domain/template/service/Template.Service.ts
@@ -95,27 +95,36 @@ export class TemplateService {
             )});
     }
 
-    private mergeForAllManyTemplates(affiliationData: Affiliation, userTemplateDatas: UserTemplate[], questionDatas: Question[], userChallengeDatas:UserChallenge[]) {
+    private mergeForAllManyTemplates(
+        affiliationData: Affiliation, 
+        userTemplateDatas: UserTemplate[], 
+        questionDatas: Question[], 
+        userChallengeDatas: UserChallenge[]
+    ):TemplateContent[][]{
         return userTemplateDatas.map(userTemplateData => {
-            return questionDatas.map(questionData => {
+            return questionDatas.reduce((acc, questionData) => {
                 const questionContent = userTemplateData.getQuestionContents().find((content) => content.getQuestionId() === questionData.getId());
                 const userChallengeData = userChallengeDatas.find((content) => content.getId() === userTemplateData.getUserChallengeId());
                 const myLikeSign = userTemplateData.likes.some((like) => like.getAffiliationId() === affiliationData.getId()) ? '1' : '0';
-                const userChallengeAfiliation = userChallengeData.getAffiliation()
-                return TemplateContent.of(
-                    userChallengeAfiliation,
-                    questionData.getId(),
-                    userTemplateData.getId(),
-                    questionContent,
-                    formatDate(userTemplateData.getCreatedAt().toString()),
-                    questionData,
-                    userTemplateData.getLikes().length.toString(),
-                    userTemplateData.getComments().length.toString(),
-                    myLikeSign
-                );
-            });
+                if (questionContent && userChallengeData) {
+                    const userChallengeAffiliation = userChallengeData.getAffiliation();
+                    acc.push(TemplateContent.of(
+                        userChallengeAffiliation,
+                        questionData.getId(),
+                        userTemplateData.getId(),
+                        questionContent,
+                        formatDate(userTemplateData.getCreatedAt().toString()),
+                        questionData,
+                        userTemplateData.getLikes().length.toString(),
+                        userTemplateData.getComments().length.toString(),
+                        myLikeSign
+                    ));
+                }
+                return acc;
+            }, []);
         });
     }
+    
 
     private extractUserChallengeId(userChallenge:UserChallenge[]){
         return userChallenge.map((data)=> data.getId())
@@ -136,8 +145,8 @@ export class TemplateService {
         return TemplateInformation.of(undefined, sortedCompanyData);
     }
 
-    
-    private mergeForMyManyTemplates(affiliationData: Affiliation, userTemplateDatas: UserTemplate[], questionDatas: Question[]) {
+
+    private mergeForMyManyTemplates(affiliationData: Affiliation, userTemplateDatas: UserTemplate[], questionDatas: Question[]):TemplateContent[][] {
         return userTemplateDatas.map(userTemplateData => {
             return questionDatas.reduce((acc, questionData) => {
                 const questionContent = userTemplateData.getQuestionContents().find((content) => content.getQuestionId() === questionData.getId());
