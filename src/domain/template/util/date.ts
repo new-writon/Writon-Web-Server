497119dd551dbd6f8ca55e85cb0831b42f8ebc18
seungryeol <opop0421@naver.com>
2024-09-02 04:50:48 +0900

    [fix] date 함수 변경

--- src/domain/template/util/date.ts
+++ src/domain/template/util/date.ts
@@ -1,8 +1,7 @@
 export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const utcDate = new Date(date.getTime() - 9 * 60 * 60 * 1000); 
-    const year = utcDate.getUTCFullYear();
-    const month = String(utcDate.getUTCMonth() + 1).padStart(2, '0'); 
-    const day = String(utcDate.getUTCDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}T00:00:00.000Z`; 
 };
