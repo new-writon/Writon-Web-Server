5140b207629465313475d0a7c4dee5881aa4fd5d
seungryeol <opop0421@naver.com>
2024-09-02 11:45:09 +0900

    [fix] redis 환경변수 변경

--- src/global/config/RedisConfig.ts
+++ src/global/config/RedisConfig.ts
@@ -1,16 +1,21 @@
 import { CacheModuleOptions, CacheOptionsFactory } from '@nestjs/cache-manager';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import redisStore from 'cache-manager-redis-store';
 
 
 @Injectable()
 export class RedisConfig implements CacheOptionsFactory{
+
+  constructor(
+    private readonly configService: ConfigService,
+){}
     
     createCacheOptions(): CacheModuleOptions {
         const config: CacheModuleOptions = {
           store: redisStore,
-          host: process.env.AWS_REDIS_ENDPOINT,
-          port: process.env.AWS_REDIS_PORT,
+          host: this.configService.get<string>('redis.host'),
+          port: this.configService.get<string>('redis.port'),
         };
         return config;
       }
