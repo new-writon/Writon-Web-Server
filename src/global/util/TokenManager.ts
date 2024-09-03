9a5d81c39c78ed0789f1fceba7b6454afdc34088
seungryeol <opop0421@naver.com>
2024-09-03 12:00:43 +0900

    [fix] 토큰 함수 변경

--- src/global/util/TokenManager.ts
+++ src/global/util/TokenManager.ts
@@ -8,8 +8,8 @@ export class TokenManager {
         @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
     ) {}
 
-    public async setToken(key:string, value:string | string[], time:number){   
-        await this.cacheManager.set(key, value, time);
+    public async setToken(key:string, value:string | string[], time:number){
+        this.setData(key, value, time);
     }
 
     public async getToken(key:string): Promise<string | string[]>{   
@@ -19,6 +19,18 @@ export class TokenManager {
 
     public async deleteToken(key:string){
         await this.cacheManager.del(key);
+    }
+
+    public async setData(key:string, value:string | string[], time:number){
+        switch(true){
+            case (typeof value == 'string'):
+                const data = await this.getToken(key) as string[];
+                data.push(value);
+        
+            case (Array.isArray(value) && value.every(item => typeof item === 'string')):
+                await this.cacheManager.set(key, value, time);
+                console.log(await this.getToken(key))
+        }
     }
 
 
