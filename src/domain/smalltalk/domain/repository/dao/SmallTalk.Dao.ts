4a63f763c21fa8f41c238dc7523ccef31c29b743
seungryeol <opop0421@naver.com>
2024-09-11 02:38:56 +0900

    [fix] 쿼리 튜닝

--- src/domain/smalltalk/domain/repository/dao/SmallTalk.Dao.ts
+++ src/domain/smalltalk/domain/repository/dao/SmallTalk.Dao.ts
@@ -12,18 +12,21 @@ export class SmallTalkDao extends Repository<SmallTalk> implements SmallTalkRepo
     constructor(private dataSource: DataSource) { super(SmallTalk, dataSource.createEntityManager()); }
 
     async findParticularSmallTalkByChallengeIdAndDate(challengeId:number, date:string):Promise<ParticularSmallTalkData[]>{
-        const particularSmallTalkData :ParticularSmallTalkData[] = await this.dataSource.createQueryBuilder()
+        const startDate = `${date} 00:00:00`;
+        const endDate = `${date} 23:59:59`;
+        const particularSmallTalkData: ParticularSmallTalkData[] = await this.dataSource.createQueryBuilder()
             .select([
                 'st.small_talk_id AS smallTalkId',
                 'st.question AS question',
                 'COUNT(DISTINCT stc.affiliation_id) AS participateCount',
                 "DATE_FORMAT(st.created_at, '%H:%i') AS createdTime",
-                "DATE(st.created_at) AS createdDate",
+                `'${date}' AS createdDate`,  
                 'st.user_challenge_id AS userChallengeId'
             ])
             .from(SmallTalk, 'st')
             .leftJoin(SmallTalkComment, 'stc', 'stc.small_talk_id = st.small_talk_id')
-            .where('DATE(st.created_at) = :date', { date })
+            .where('st.created_at >= :startDate', { startDate })
+            .andWhere('st.created_at <= :endDate', { endDate })
             .andWhere('st.challenge_id = :challengeId', { challengeId })
             .groupBy('st.small_talk_id, st.question, st.created_at, st.user_challenge_id')
             .orderBy('st.created_at', 'DESC')
