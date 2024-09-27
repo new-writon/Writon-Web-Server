74f4336b7646a92b20120dd5c91c186a49e113a5
seungryeol <opop0421@naver.com>
2024-09-27 17:08:09 +0900

    [feat] 파이어베이스 연결

---
+++ src/global/config/firebase.ts
@@ -0,0 +1,7 @@
+
+import firebaseAdmin from 'firebase-admin';
+import googleService from '../../../writon-firebase-admin.json';
+
+export const firebase = firebaseAdmin.initializeApp({
+	credential : firebaseAdmin.credential.cert(googleService as firebaseAdmin.ServiceAccount)
+});
