f0e3bf5e581503f564bb32953f0d9970d059bb1e
seungryeol <opop0421@naver.com>
2024-10-24 21:53:27 +0900

    [fix] 상세 템플릿 조회 api 로직 변경

--- src/domain/template/domain/repository/UserTemplate.Repository.ts
+++ src/domain/template/domain/repository/UserTemplate.Repository.ts
@@ -1,22 +1,34 @@
-import {  Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { UserTemplate } from '../entity/UserTemplate';
-import { TemplateContent } from '../../dto/response/TemplateContent';
-
-
-
 
 export interface UserTemplateRepository extends Repository<UserTemplate> {
-
-   findUserTemplateByUserChallengeId(userChallengeId: number): Promise<UserTemplate[]>;
-   findChallengeSuccessChallengeCount(userChallengeId:number): Promise<number>;
-   insertUserTemplate(userChallnegeId: number,date: Date, complete: boolean): Promise<UserTemplate>;
-   findUserTemplateAndCommentAndLikeByUserChallengeId(userChallengeId:number):Promise<UserTemplate[]>;
-   findUserTemplateAndCommentAndLikeAndQeustionContentByUserChallengeIdAndDate(userChallengeId:number[], date:Date):Promise<UserTemplate[]>;
-   findUserTemplateAndCommentAndLikeAndQeustionContentByUserChallengeId(userChallengeId:number):Promise<UserTemplate[]>;
-   findUserTemplateAndCommentAndLikeAndQeustionContentByUserTemplateIdWithVisibility(userTemplateId:number, visibility:boolean):Promise<UserTemplate>;
-   findUserTemplateSuccessCountByUserChallengeIds(userChallengeIds: number[]);
-   findUserTemplateByUserChallengeIdAndDate(userChallengeId:number, date:string):Promise<UserTemplate>;
-   findUserTemplateById(userTemplateId:number):Promise<UserTemplate>;
-
-  
+  findUserTemplateByUserChallengeId(
+    userChallengeId: number,
+  ): Promise<UserTemplate[]>;
+  findChallengeSuccessChallengeCount(userChallengeId: number): Promise<number>;
+  insertUserTemplate(
+    userChallnegeId: number,
+    date: Date,
+    complete: boolean,
+  ): Promise<UserTemplate>;
+  findUserTemplateAndCommentAndLikeByUserChallengeId(
+    userChallengeId: number,
+  ): Promise<UserTemplate[]>;
+  findUserTemplateAndCommentAndLikeAndQeustionContentByUserChallengeIdAndDate(
+    userChallengeId: number[],
+    date: Date,
+  ): Promise<UserTemplate[]>;
+  findUserTemplateAndCommentAndLikeAndQeustionContentByUserChallengeId(
+    userChallengeId: number,
+  ): Promise<UserTemplate[]>;
+  findUserTemplateAndCommentAndLikeAndQeustionContentByUserTemplateIdWithVisibility(
+    userTemplateId: number,
+    visibility: boolean,
+  ): Promise<UserTemplate>;
+  findUserTemplateSuccessCountByUserChallengeIds(userChallengeIds: number[]);
+  findUserTemplateByUserChallengeIdAndDate(
+    userChallengeId: number,
+    date: string,
+  ): Promise<UserTemplate>;
+  findUserTemplateById(userTemplateId: number): Promise<UserTemplate>;
 }
