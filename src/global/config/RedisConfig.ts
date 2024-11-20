7b6acecb48934b6afa2e5cf6fc78ae36f8b789ef
seungryeol <opop0421@naver.com>
2024-11-21 00:17:19 +0900

    [fix] 토큰 재발급 로직 변경

--- src/global/config/RedisConfig.ts
+++ src/global/config/RedisConfig.ts
@@ -12,7 +12,7 @@ export class RedisConfig implements CacheOptionsFactory {
       store: redisStore,
       host: this.configService.get<string>('redis.host'),
       port: this.configService.get<string>('redis.port'),
-      ttl: 0,
+      ttl: undefined,
     };
     return config;
   }
