4e88f5db27f17b4d45d875856fc68cd08e67f5e1
seungryeol <opop0421@naver.com>
2024-11-21 00:02:22 +0900

    [fix] 토큰 재발급 로직 변경

--- src/global/util/CacheImpl.ts
+++ src/global/util/CacheImpl.ts
@@ -11,8 +11,7 @@ export class CacheImpl implements CacheManager {
     value: string | string[],
     ttl: number,
   ): Promise<void> {
-    console.log(ttl);
-    await this.cacheManager.set(key, value, 0);
+    await this.cacheManager.set(key, value, ttl);
   }
 
   public async getValue(key: string): Promise<string | string[] | undefined> {
