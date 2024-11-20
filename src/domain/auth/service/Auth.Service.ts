e2b833482925cd6a67f0e1710423229af65f8810
seungryeol <opop0421@naver.com>
2024-11-20 16:31:16 +0900

    [fix] 토큰 재발급 로직 변경

--- src/domain/auth/service/Auth.Service.ts
+++ src/domain/auth/service/Auth.Service.ts
@@ -10,14 +10,10 @@ import { Affiliation } from '../../user/domain/entity/Affiliation';
 import { UserApi } from '../intrastructure/User.Api';
 import { AuthVerifyService } from '../../../global/exception/auth/AuthVerify.Service';
 import { LoginTokenManager } from '../util/LoginTokenManager';
-import { LocalLogin } from '../dto/request/LocalLogin';
-import { DataSource } from 'typeorm';
-import { Transactional } from '../../..//global/decorator/transaction';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly dataSource: DataSource,
     private readonly socialLogin: SocialLogin,
     private readonly jwtManager: JwtManager,
     private readonly loginTokenManager: LoginTokenManager,
@@ -25,7 +21,6 @@ export class AuthService {
     private readonly authVerifyService: AuthVerifyService,
   ) {}
 
-  @Transactional()
   public async kakaoLogin(
     organization: string,
     challengeId: number,
@@ -49,7 +44,7 @@ export class AuthService {
     await this.loginTokenManager.setToken(
       String(checkedUserData.getId()),
       [refreshToken],
-      //   30 * 24 * 60 * 60,
+      30 * 24 * 60 * 60,
     );
     let [affiliatedConfirmation, challengedConfirmation] = await Promise.all([
       this.checkAffiliationStatus(organization, checkedUserData.getId()),
@@ -72,15 +67,17 @@ export class AuthService {
     );
   }
 
-  @Transactional()
-  public async localLogin(loginLocal: LocalLogin): Promise<LoginResponse> {
+  public async localLogin(
+    identifier: string,
+    password: string,
+    organization: string,
+    challengeId: number,
+  ): Promise<LoginResponse> {
     const userData: User =
-      await this.userApi.requestUserDataBySocialNumberOrIdentifier(
-        loginLocal.getIdentifier(),
-      );
+      await this.userApi.requestUserDataBySocialNumberOrIdentifier(identifier);
     this.authVerifyService.vefifyIdentifier(userData);
     await this.authVerifyService.verifyPassword(
-      loginLocal.getPassword(),
+      password,
       userData.getPassword(),
     );
     const accessToken = this.jwtManager.makeAccessToken(
@@ -91,21 +88,14 @@ export class AuthService {
     await this.loginTokenManager.setToken(
       String(userData.getId()),
       [refreshToken],
-      //  30 * 24 * 60 * 60 * 2,
+      30 * 24 * 60 * 60,
     );
     let [affiliatedConfirmation, challengedConfirmation] = await Promise.all([
-      this.checkAffiliationStatus(
-        loginLocal.getOrganization(),
-        userData.getId(),
-      ),
-      this.checkOngoingChallenge(
-        loginLocal.getOrganization(),
-        userData.getId(),
-        loginLocal.getChallengeId(),
-      ),
+      this.checkAffiliationStatus(organization, userData.getId()),
+      this.checkOngoingChallenge(organization, userData.getId(), challengeId),
     ]);
     affiliatedConfirmation = this.checkOrganization(
-      loginLocal.getOrganization(),
+      organization,
       affiliatedConfirmation,
     );
     return LoginResponse.of(
@@ -117,36 +107,9 @@ export class AuthService {
     );
   }
 
-  // private async proccessFirebaseTokenLogic(deviceType:string, userData:User, engineValue:string){
-  //     if(deviceType === 'phone'){
-  //         const firebaseTokenData = await this.userApi.requestFirebaseTokenByUserIdAndEngineValue(userData.getId(), engineValue);
-  //         const flag = checkData(firebaseTokenData);
-  //         await this.insertFirebaseTokenIfNotExists(flag, userData.getId(), engineValue);
-  //     }
-  // }
-
-  // private checkDeviceType(engineValue:string){
-  //     if(engineValue === "null") return 'computer';
-  //     return 'phone';
-  // }
-
-  // private async insertFirebaseTokenIfNotExists(flag:boolean, userId:number, engineValue:string){
-  //     if(!flag){ await this.userApi.executeInsertFirebaseToken(userId, engineValue);}
-  // }
-
-  @Transactional()
-  public async logout(
-    userId: string,
-    refreshToken: string,
-    engineValue: string,
-  ): Promise<void> {
+  public async logout(userId: string, refreshToken: string): Promise<void> {
     await this.loginTokenManager.deleteToken(userId, refreshToken);
-    await this.userApi.executeDeleteFirebaseToken(Number(userId), engineValue);
   }
-
-  // private async deleteFirebaseTokenByDeviceType(deviceType:string, userId:number, engineValue:string){
-  //     if(deviceType === 'phone'){ await this.userApi.executeDeleteFirebaseToken(userId, engineValue);}
-  // }
 
   private checkOrganization(
     organization: string,
