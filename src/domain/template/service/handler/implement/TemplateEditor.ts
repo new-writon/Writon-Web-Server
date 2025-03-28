c20f1820953587ed777cfe578b10c87fd59fe56d
seungryeol <opop0421@naver.com>
2025-03-28 15:28:00 +0900

    [fix] 버그 수정

--- src/domain/template/service/handler/implement/TemplateEditor.ts
+++ src/domain/template/service/handler/implement/TemplateEditor.ts
@@ -5,24 +5,31 @@ import { TemplateOperation } from '../../types/Operation';
 import { Transactional } from 'src/global/decorator/transaction';
 import { QuestionContentHelper } from '../../../helper/QuestionContent.Helper';
 import { TemplateWriter } from '../TemplateWriter';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class TemplateEditor
   extends TemplateWriter
-  implements TemplateHandler<TemplateUpdate, Promise<void>>
+  implements TemplateHandler<[TemplateUpdate], Promise<void>>
 {
   operation: TemplateOperation = 'UPDATE_TEMPLATE';
 
-  constructor(private readonly questionContentHelper: QuestionContentHelper) {
+  constructor(
+    private readonly questionContentHelper: QuestionContentHelper,
+    private readonly dataSource: DataSource,
+  ) {
     super();
   }
 
   @Transactional()
-  async handle(request: TemplateUpdate): Promise<void> {
-    await this.questionContentHelper.executeDeleteQuestionContent(request.getUserTemplateId());
+  async handle(request: [TemplateUpdate]): Promise<void> {
+    const [templateUpdate] = request;
+    await this.questionContentHelper.executeDeleteQuestionContent(
+      templateUpdate.getUserTemplateId(),
+    );
     const changedTemplate = super.changeUserTemplateType(
-      request.getTemplateContent(),
-      request.getUserTemplateId(),
+      templateUpdate.getTemplateContent(),
+      templateUpdate.getUserTemplateId(),
     );
     await this.questionContentHelper.executeInsertQuestionContent(changedTemplate);
   }
