c9c72c3991bacc81e109cadb9d0d14c5541071ed
seungryeol <opop0421@naver.com>
2025-04-27 00:19:27 +0900

    ..

--- src/domain/template/interface/GetLikeNotify.ts
+++ src/domain/template/interface/GetLikeNotify.ts
@@ -10,14 +10,3 @@ interface GetLikeNotify {
 }
 
 export { GetLikeNotify };
-=======
-// interface GetLikeNotify {
-//   likeId: number;
-//   createdAt: Date;
-//   sign: number;
-//   userTemplateId: number;
-//   templateName: Date;
-//   nickname: string;
-//   type: string;
-// }
->>>>>>> 2278913d0355cf8219ccb5e3c3f0ddb96354cc5e
